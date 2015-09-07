11264345ac70c99f590e10da54062ec24b1c6fe4
Dustin Specker <dustinspecker@dustinspecker.com>
2015-09-07 09:11:59 -0500

    feat(vfile-reporter): report all files
    Closes #1
    
    Delete a lot of formatting functionality. vfile-reporter takes care
    of all of it.
    
    In the future, add support for sending options to reporter.

--- src/index.js
+++ src/index.js
@@ -1,43 +1,12 @@
 'use strict';
 import alex from 'alex';
 import convertVinylToVfile from 'convert-vinyl-to-vfile';
-import indentString from 'indent-string';
-import {log} from 'gulp-util';
+import reporter from 'vfile-reporter';
 import through from 'through2';
-
-/**
- * Return number of digits in line + column
- * @param {Object} message - message to analyze
- * @param {Number} message.column - column number of warning
- * @param {Number} message.line - line number of warning
- * @return {Number} - number of digits in message's line and column
- */
-function getLineColLength(message) {
-  return (message.column.toString() + message.line.toString()).length;
-}
-
-/**
- * Return the max getLineColLength for a list of messages
- * @param {Object[]} messages - list of messages
- * @return {Number} - max line + col length
- */
-function getMaxLineColLength(messages) {
-  let maxLineColLength = 0;
-
-  messages.forEach((message) => {
-    const lineColLength = getLineColLength(message);
-
-    if (lineColLength > maxLineColLength) {
-      maxLineColLength = lineColLength;
-    }
-  });
-
-  return maxLineColLength;
-}
 
 export default function gulpAlex() {
   return through.obj(function (file, encoding, callback) {
-    let convertedFile, maxLineColLength;
+    let convertedFile;
 
     if (!file || file.isNull()) {
       this.push();
@@ -47,24 +16,7 @@ export default function gulpAlex() {
     convertedFile = convertVinylToVfile(file);
 
     alex(convertedFile);
-
-    if (convertedFile.messages.length > 0) {
-      maxLineColLength = getMaxLineColLength(convertedFile.messages);
-
-      log(convertedFile.filename + '.' + convertedFile.extension);
-
-      convertedFile.messages.forEach((message) => {
-        let formattedReason, numOfSpaces;
-
-        // always indent at least 2 spaces
-        numOfSpaces = 2 + maxLineColLength - getLineColLength(message);
-
-        formattedReason = `${message.line}:${message.column} ${message.reason}`;
-        formattedReason = indentString(formattedReason, ' ', numOfSpaces);
-
-        log(formattedReason);
-      });
-    }
+    console.log(reporter([convertedFile]));
 
     callback(null, file);
   });
--- test/test.js
+++ test/test.js
@@ -7,19 +7,13 @@ import proxyquire from 'proxyquire';
 import sinon from 'sinon';
 
 describe('gulp-alex', () => {
-  let fileWithOneError, fileWithTwoErrors, validFile;
+  let fileWithOneError, validFile;
 
   beforeEach(() => {
     fileWithOneError = new File({
       base: './',
       path: join('users', 'dustin', 'project', 'awesome.project.md'),
       contents: new Buffer('Garbagemen versus Abe Lincoln!')
-    });
-
-    fileWithTwoErrors = new File({
-      base: './',
-      path: join('users', 'dustin', 'project', 'awesome.project.md'),
-      contents: new Buffer('Garbagemen versus Abe Lincoln! Be like a man.')
     });
 
     validFile = new File({
@@ -29,20 +23,18 @@ describe('gulp-alex', () => {
     });
   });
 
-  it('should not log if no files passed', (done) => {
-    let alexProxy, gutilStub, stream;
+  it('should not report if no files passed', (done) => {
+    let alexProxy, reporter, stream;
 
-    gutilStub = {
-      log: sinon.stub()
-    };
+    reporter = sinon.stub();
 
-    alexProxy = proxyquire('../lib', {'gulp-util': gutilStub});
+    alexProxy = proxyquire('../lib', {'vfile-reporter': reporter});
 
     stream = alexProxy();
 
     stream.on('data', (file) => {
       expect(file).to.eql(undefined);
-      expect(gutilStub.log.called).to.eql(false);
+      expect(reporter.called).to.eql(false);
       done();
     });
 
@@ -51,20 +43,18 @@ describe('gulp-alex', () => {
     stream.end();
   });
 
-  it('should not log if no errors found', (done) => {
-    let alexProxy, gutilStub, stream;
+  it('should report if an error is not found', (done) => {
+    let alexProxy, reporter, stream;
 
-    gutilStub = {
-      log: sinon.stub()
-    };
+    reporter = sinon.stub();
 
-    alexProxy = proxyquire('../lib', {'gulp-util': gutilStub});
+    alexProxy = proxyquire('../lib', {'vfile-reporter': reporter});
 
     stream = alexProxy();
 
     stream.on('data', (file) => {
       expect(file).to.eql(validFile);
-      expect(gutilStub.log.called).to.eql(false);
+      expect(reporter.called).to.eql(true);
       done();
     });
 
@@ -73,49 +63,22 @@ describe('gulp-alex', () => {
     stream.end();
   });
 
-  it('should log if an error is found', (done) => {
-    let alexProxy, gutilStub, stream;
+  it('should report if an error is found', (done) => {
+    let alexProxy, reporter, stream;
 
-    gutilStub = {
-      log: sinon.stub()
-    };
+    reporter = sinon.stub();
 
-    alexProxy = proxyquire('../lib', {'gulp-util': gutilStub});
+    alexProxy = proxyquire('../lib', {'vfile-reporter': reporter});
 
     stream = alexProxy();
 
     stream.on('data', (file) => {
       expect(file).to.eql(fileWithOneError);
-      expect(gutilStub.log.withArgs('awesome.project.md').calledOnce).to.eql(true);
-      expect(gutilStub.log.withArgs('  1:1 `Garbagemen` may be insensitive, use `Garbage collectors`, `Waste collectors`, `Trash collectors` instead').calledOnce).to.eql(true);
+      expect(reporter.called).to.eql(true);
       done();
     });
 
     stream.write(fileWithOneError);
-
-    stream.end();
-  });
-
-  it('should right align row/col numbers for multiple errors', (done) => {
-    let alexProxy, gutilStub, stream;
-
-    gutilStub = {
-      log: sinon.stub()
-    };
-
-    alexProxy = proxyquire('../lib', {'gulp-util': gutilStub});
-
-    stream = alexProxy();
-
-    stream.on('data', (file) => {
-      expect(file).to.eql(fileWithTwoErrors);
-      expect(gutilStub.log.withArgs('awesome.project.md').calledOnce).to.eql(true);
-      expect(gutilStub.log.withArgs('   1:1 `Garbagemen` may be insensitive, use `Garbage collectors`, `Waste collectors`, `Trash collectors` instead').calledOnce).to.eql(true);
-      expect(gutilStub.log.withArgs('  1:35 `like a man` may be insensitive, use `resolutely`, `bravely` instead').calledOnce).to.eql(true);
-      done();
-    });
-
-    stream.write(fileWithTwoErrors);
 
     stream.end();
   });

fe72a8e8a44b81176f6a443935450064db40f83c
Dustin Specker <dustinspecker@dustinspecker.com>
2016-01-10 15:52:10 -0600

    chore(deps): update eslint-config-dustinspecker

--- src/index.js
+++ src/index.js
@@ -1,73 +1,73 @@
 /* eslint no-invalid-this: 0 */
-'use strict';
-import alex from 'alex';
-import convertVinylToVfile from 'convert-vinyl-to-vfile';
-import {PluginError} from 'gulp-util';
-import reporter from 'vfile-reporter';
-import through from 'through2';
+'use strict'
+import alex from 'alex'
+import convertVinylToVfile from 'convert-vinyl-to-vfile'
+import {PluginError} from 'gulp-util'
+import reporter from 'vfile-reporter'
+import through from 'through2'
 
 module.exports = function () {
   return through.obj(function (file, encoding, callback) {
     if (!file || file.isNull()) {
-      this.push();
-      return callback();
+      this.push()
+      return callback()
     }
 
-    file.alex = alex(file.contents.toString());
+    file.alex = alex(file.contents.toString())
 
-    callback(null, file);
-  });
-};
+    callback(null, file)
+  })
+}
 
 module.exports.reporter = function (reporterType) {
   const failedFiles = []
     , isDefaultReporter = !reporterType
     , isFailReporter = reporterType === 'fail'
-    , isFailImmediatelyReporter = reporterType === 'failImmediately';
+    , isFailImmediatelyReporter = reporterType === 'failImmediately'
 
   return through.obj(function (file, encoding, callback) {
     let error = null
       , foundIssues = false
-      , convertedFile, report;
+      , convertedFile, report
 
     if (!file || file.isNull()) {
-      this.push();
-      return callback();
+      this.push()
+      return callback()
     }
 
-    convertedFile = convertVinylToVfile(file);
-    convertedFile.messages = file.alex.messages;
+    convertedFile = convertVinylToVfile(file)
+    convertedFile.messages = file.alex.messages
 
     // default report to console
     if (isDefaultReporter) {
-      report = reporter(convertedFile, {quiet: true});
+      report = reporter(convertedFile, {quiet: true})
       if (report) {
-        console.log(report);
+        console.log(report)
       }
     }
 
-    foundIssues = !!file.alex.messages.length;
+    foundIssues = !!file.alex.messages.length
 
     if (isFailImmediatelyReporter && foundIssues) {
       error = new PluginError('gulp-alex', {
         name: 'AlexError',
         message: `Alex failed for ${file.path}`
-      });
+      })
     }
 
     if (reporterType === 'fail' && foundIssues) {
-      failedFiles.push(file.path);
+      failedFiles.push(file.path)
     }
 
-    callback(error, file);
+    callback(error, file)
   }, function (callback) {
     if (isFailReporter && failedFiles.length) {
       this.emit('error', new PluginError('gulp-alex', {
         name: 'AlexError',
         message: `Alex failed for ${failedFiles.join(', ')}`
-      }));
+      }))
     }
 
-    callback();
-  });
-};
+    callback()
+  })
+}
--- test/test.js
+++ test/test.js
@@ -1,141 +1,141 @@
 /* global describe, beforeEach, it */
-'use strict';
-import austin from 'austin';
-import {expect} from 'chai';
-import {File} from 'gulp-util';
-import {join} from 'path';
-import proxyquire from 'proxyquire';
+'use strict'
+import austin from 'austin'
+import {expect} from 'chai'
+import {File} from 'gulp-util'
+import {join} from 'path'
+import proxyquire from 'proxyquire'
 
 describe('gulp-alex', () => {
-  const filePath = join('users', 'dustin', 'project', 'awesome.project.md');
-  let alexProxy, fileWithOneError, reporter, stream, validFile;
+  const filePath = join('users', 'dustin', 'project', 'awesome.project.md')
+  let alexProxy, fileWithOneError, reporter, stream, validFile
 
   beforeEach(() => {
-    reporter = austin.spy();
-    alexProxy = proxyquire('../lib', {'vfile-reporter': reporter});
+    reporter = austin.spy()
+    alexProxy = proxyquire('../lib', {'vfile-reporter': reporter})
 
-    stream = alexProxy();
+    stream = alexProxy()
 
     fileWithOneError = new File({
       base: './',
       path: filePath,
       contents: new Buffer('Garbagemen versus Abe Lincoln!')
-    });
+    })
 
     validFile = new File({
       base: './',
       path: filePath,
       contents: new Buffer('Abe Lincoln is awesome.')
-    });
-  });
+    })
+  })
 
   it('should not report if no files passed', done => {
     stream
       .pipe(alexProxy.reporter())
       .on('data', file => {
-        expect(file).to.eql(undefined);
-        expect(reporter.callCount()).to.eql(0);
-        done();
-      });
+        expect(file).to.eql(undefined)
+        expect(reporter.callCount()).to.eql(0)
+        done()
+      })
 
-    stream.write();
+    stream.write()
 
-    stream.end();
-  });
+    stream.end()
+  })
 
   it('should report if an error is not found', done => {
     stream
       .pipe(alexProxy.reporter())
       .on('data', file => {
-        expect(file).to.eql(validFile);
-        expect(reporter.callCount()).to.eql(1);
-        done();
-      });
+        expect(file).to.eql(validFile)
+        expect(reporter.callCount()).to.eql(1)
+        done()
+      })
 
-    stream.write(validFile);
+    stream.write(validFile)
 
-    stream.end();
-  });
+    stream.end()
+  })
 
   it('should report if an error is found', done => {
     stream
       .pipe(alexProxy.reporter())
       .on('data', file => {
-        expect(file).to.eql(fileWithOneError);
-        expect(reporter.callCount()).to.eql(1);
-        done();
-      });
+        expect(file).to.eql(fileWithOneError)
+        expect(reporter.callCount()).to.eql(1)
+        done()
+      })
 
-    stream.write(fileWithOneError);
+    stream.write(fileWithOneError)
 
-    stream.end();
-  });
+    stream.end()
+  })
 
   it('should not print if an error is not found', done => {
-    reporter.returns('');
-    austin.spy(global.console, 'log');
+    reporter.returns('')
+    austin.spy(global.console, 'log')
 
     stream
       .pipe(alexProxy.reporter())
       .on('data', () => {
-        expect(global.console.log.callCount()).to.eql(0);
-        global.console.log.restore();
-        done();
-      });
+        expect(global.console.log.callCount()).to.eql(0)
+        global.console.log.restore()
+        done()
+      })
 
-    stream.write(validFile);
+    stream.write(validFile)
 
-    stream.end();
-  });
+    stream.end()
+  })
 
   it('should print if an error is found', done => {
-    reporter.returns('error');
-    austin.spy(global.console, 'log');
+    reporter.returns('error')
+    austin.spy(global.console, 'log')
 
     stream
       .pipe(alexProxy.reporter())
       .on('data', () => {
-        expect(global.console.log.calledWith('error')).to.eql(true);
-        global.console.log.restore();
-        done();
-      });
+        expect(global.console.log.calledWith('error')).to.eql(true)
+        global.console.log.restore()
+        done()
+      })
 
-    stream.write(validFile);
+    stream.write(validFile)
 
-    stream.end();
-  });
+    stream.end()
+  })
 
   describe('failImmediately reporter', () => {
     it('should not emit an error if failImmediately reporter is not being used', done => {
       stream
         .pipe(alexProxy.reporter())
         .on('error', () => {
-          throw new Error('Should not emit an error');
+          throw new Error('Should not emit an error')
         })
         .on('end', done)
-        .resume();
+        .resume()
 
-      stream.write(fileWithOneError);
+      stream.write(fileWithOneError)
 
-      stream.end();
-    });
+      stream.end()
+    })
 
     it('should emit an error if using failImmediately reporter', done => {
       stream
         .pipe(alexProxy.reporter('failImmediately'))
         .on('error', error => {
-          expect(error.plugin).to.eql('gulp-alex');
+          expect(error.plugin).to.eql('gulp-alex')
 
-          expect(error.name).to.eql('AlexError');
-          expect(error.message).to.eql(`Alex failed for ${fileWithOneError.path}`);
-          done();
-        });
+          expect(error.name).to.eql('AlexError')
+          expect(error.message).to.eql(`Alex failed for ${fileWithOneError.path}`)
+          done()
+        })
 
-      stream.write(fileWithOneError);
+      stream.write(fileWithOneError)
 
-      stream.end();
-    });
-  });
+      stream.end()
+    })
+  })
 
   describe('fail reporter', () => {
     it('should not emit an error if no issues', done => {
@@ -143,33 +143,33 @@ describe('gulp-alex', () => {
         .pipe(alexProxy.reporter())
         .pipe(alexProxy.reporter('fail'))
         .on('error', () => {
-          throw new Error('Should not emit an error');
+          throw new Error('Should not emit an error')
         })
         .on('end', done)
-        .resume();
+        .resume()
 
-      stream.write(validFile);
-      stream.write(validFile);
+      stream.write(validFile)
+      stream.write(validFile)
 
-      stream.end();
-    });
+      stream.end()
+    })
 
     it('should emit an error after all files have been processed', done => {
       stream
         .pipe(alexProxy.reporter())
         .pipe(alexProxy.reporter('fail'))
         .on('error', error => {
-          expect(error.plugin).to.eql('gulp-alex');
+          expect(error.plugin).to.eql('gulp-alex')
 
-          expect(error.name).to.eql('AlexError');
-          expect(error.message).to.eql(`Alex failed for ${filePath}, ${filePath}`);
-          done();
-        });
+          expect(error.name).to.eql('AlexError')
+          expect(error.message).to.eql(`Alex failed for ${filePath}, ${filePath}`)
+          done()
+        })
 
-      stream.write(fileWithOneError);
-      stream.write(fileWithOneError);
+      stream.write(fileWithOneError)
+      stream.write(fileWithOneError)
 
-      stream.end();
-    });
-  });
-});
+      stream.end()
+    })
+  })
+})
